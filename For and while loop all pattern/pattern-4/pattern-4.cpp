b04f29a252a9a6e59a7e4361e482df8a8322c9ab
AshabAsir <ashabasir010@gmail.com>
2022-05-27 15:10:45 +0600

    pattern 10 added

--- For and while loop all pattern/pattern-4/pattern-4.cpp
+++ For and while loop all pattern/pattern-4/pattern-4.cpp
@@ -4,10 +4,12 @@ int main(){
 int n;
 cin>>n;
 int i=1;
+int count =1;
 while(i<=n){
     int j=1;
-    while(j<=i){
-        cout<<"*"<<" ";
+    while(j<=n){
+        cout<<count<<" ";
+        count++;
         j++;
     }
     i++;
