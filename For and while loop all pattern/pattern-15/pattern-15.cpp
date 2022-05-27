c8190f4a394a06df2ac2a5635d56272c0d4a682c
AshabAsir <ashabasir010@gmail.com>
2022-05-27 16:37:18 +0600

    pattern 15 updated

--- For and while loop all pattern/pattern-15/pattern-15.cpp
+++ For and while loop all pattern/pattern-15/pattern-15.cpp
@@ -5,14 +5,13 @@ int main()
     int n;
     cin>>n;
     int i=1;
-    char ch='A';
-   while(i<=n)
+    while(i<=n)
     {
         int j=1;
         while(j<=i)
         {
+            char ch='A'+i-1;
             cout<<ch<<" ";
-            ch++;
             j++;
         }
         cout<<endl;
